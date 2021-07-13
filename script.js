2c6ec20de2cd9456317c2e24a8ec6b0acd0a963f
ThaisPoma <thaisadri16@gmail.com>
2021-07-13 20:52:16 -0300

    atualizações

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*   MODO SIMPLICADO   */   
+/*   MODO SIMPLIFICADO   */   
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
